9f6e8b740135fdc5f8f0e179248de1b94f0da22e
C4tTu0n9 <tuongpche171654@fpt.edu.vn>
2024-10-01 21:59:40 +0700

    create view and table to show in4 user

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+import java.util.List;
 
 @Controller
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -22,22 +25,39 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        // List<User> arrUsers = this.userService.getAllUser();
+        // System.out.println(arrUsers);
+        model.addAttribute("eric", "test");
         model.addAttribute("catuong", "Cat Tuong dep trai");
         return "hello";
     }
 
-    @RequestMapping("/admin/user") // method GET
+    @RequestMapping("/admin/user/create") // method GET
     public String getCreatePage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User cattuong) {
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST) // method GET
+    public String getCreatePage(Model model, @ModelAttribute("newUser") User cattuong) {
         System.out.println("run here" + cattuong);
-        return "hello";
+        this.userService.handleSaveUser(cattuong);
+        return "redirect:/admin/user";
+    }
+
+    @RequestMapping(value = "/admin/user")
+    public String getUserPage(Model model) {
+        List<User> arrUsers = this.userService.getAllUser();
+        System.out.println(arrUsers);
+        model.addAttribute("userList", arrUsers);
+        return "admin/user/table-user";
+    }
+
+    @RequestMapping(value = "/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        System.out.println("check path: " + id);
+        model.addAttribute("id", id);
+        return "admin/user/show";
     }
 }
 
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -1,11 +1,33 @@
 package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+import java.util.List;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public String handleHello() {
         return "Hello form service";
+    }
+
+    public List<User> getAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
+    public User handleSaveUser(User user) {
+        User tuong = this.userRepository.save(user);
+        System.out.println(tuong);
+        return tuong;
     }
 }
